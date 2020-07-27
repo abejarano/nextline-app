da068d003c394df6f6503d175204e963f878f292
Oscar <oacs1221@gmail.com>
2020-07-27 18:57:45 -0400

    Added global style theme

--- App/Containers/LoginClient/index.js
+++ App/Containers/LoginClient/index.js
@@ -3,6 +3,7 @@ import {StyleSheet, View, Text, Button, TextInput} from 'react-native';
 import {login} from '../../actions/auth';
 import {useDispatch} from 'react-redux';
 import {ButtonStyled} from '../../Components/button';
+import theme from '../../styles';
 const LoginClient = () => {
   const dispatch = useDispatch();
   return (
@@ -13,8 +14,8 @@ const LoginClient = () => {
       <TextInput>clave</TextInput>
       <ButtonStyled
         onPress={() => dispatch(login({email: '', password: ''}))}
-        backgroundColor="#57585a"
-        color="#f5f5f5"
+        backgroundColor={theme.GRAY_COLOR}
+        color={theme.WHITE_COLOR}
         text={'Iniciar Sesion'}
       />
       <Text>Olvido su contrase;a?</Text>
