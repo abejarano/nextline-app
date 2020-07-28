715a674d83b269b8814b58c6e4cae6919c2579ee
Oscar <oacs1221@gmail.com>
2020-07-28 17:19:23 -0400

    Add image library

--- App/Containers/RegisterClient/index.js
+++ App/Containers/RegisterClient/index.js
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet, TextInput} from 'react-native';
+import {View, Text, Button, StyleSheet, Image} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {setSignupPartialData} from '../../actions/auth';
 import globalStyles from '../../styles';
 import {InputStyled} from '../../Components/input';
 import {ButtonStyled} from '../../Components/button';
+import ImagePicker from 'react-native-image-picker';
+import {PermissionsAndroid} from 'react-native';
+
+const options = {
+  title: 'Seleccionar Avatar',
+  // storageOptions: {
+  //   skipBackup: true,
+  //   path: 'images',
+  // },
+};
 
 export function RegisterScreen({navigation}) {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
+  const [image, setImage] = useState(null);
   const [password, setPassword] = useState('12341234');
   const [cedularif, setCedulaRif] = useState('');
   const [nombrerzb, setNombreRzb] = useState('');
@@ -17,6 +28,57 @@ export function RegisterScreen({navigation}) {
 
   return (
     <View style={styles.view}>
+      {image ? (
+        <Image
+          source={image}
+          style={{
+            borderColor: 'red',
+            borderRadius: 100,
+            height: 150,
+            width: 150,
+          }}
+        />
+      ) : (
+        <Button
+          title="Image"
+          onPress={async () => {
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+              {
+                title: 'We need your permission',
+              },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              /**
+               * The first arg is the options object for customization (it can also be null or omitted for default options),
+               * The second arg is the callback which sends object: response (more info in the API Reference)
+               */
+              ImagePicker.showImagePicker(options, (response) => {
+                console.log('Response = ', response);
+
+                if (response.didCancel) {
+                  console.log('User cancelled image picker');
+                } else if (response.error) {
+                  console.log('ImagePicker Error: ', response.error);
+                } else if (response.customButton) {
+                  console.log(
+                    'User tapped custom button: ',
+                    response.customButton,
+                  );
+                } else {
+                  // const source = {uri: response.uri};
+
+                  // You can also display the image using data:
+                  const source = response;
+                  setImage(source);
+                  console.log(source);
+                }
+              });
+            }
+          }}
+        />
+      )}
+
       <Button
         title="back"
         onPress={() => {
@@ -98,5 +160,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    height: '50px',
   },
 });
