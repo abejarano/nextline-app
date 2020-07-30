83ba47ec3c2b731d620abbd82c522abf9f14b39c
Moises Serrano <moyca777@gmail.com>
2020-07-30 00:02:31 -0300

    setting safearea in profile

--- App/Containers/Profile/index.js
+++ App/Containers/Profile/index.js
@@ -7,6 +7,7 @@ import {
   Image,
   Platform,
   KeyboardAvoidingView,
+  SafeAreaView,
 } from 'react-native';
 import {useDispatch} from 'react-redux';
 import {setSignupPartialData, signout} from '../../actions/auth';
@@ -15,7 +16,7 @@ import {InputStyled} from '../../Components/input';
 import {ButtonStyled} from '../../Components/button';
 import ImagePicker from 'react-native-image-picker';
 import {PermissionsAndroid} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 
 const options = {
   title: 'Seleccionar Avatar',
@@ -41,152 +42,154 @@ export function ProfileScreen({navigation}) {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.keyboardContainer}
-      keyboardVerticalOffset={8}>
-      <ScrollView style={styles.keyboardContainer}>
-        <View style={styles.view}>
-          {imageModeUri ? (
-            <Image source={image} style={styles.image} />
-          ) : (
-            <Image source={{uri: user.image}} style={styles.image} />
-          )}
+    <>
+      {/* <SafeAreaView style={{ flex: 0, backgroundColor: globalStyles.PRIMARY_COLOR }} /> */}
+      {/* <SafeAreaView style={{ flex: 1, backgroundColor: 'blue' }}></SafeAreaView> */}
+      
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardContainer}
+        keyboardVerticalOffset={8}>
 
-          <Button
-            title="Editar foto de perfil"
-            onPress={async () => {
-              const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-                {
-                  title: 'We need your permission',
-                },
-              );
+        <ScrollView style={styles.keyboardContainer}>
+      
+          <View style={styles.view}>
+            {imageModeUri ? (
+              <Image source={image} style={styles.image} />
+            ) : (
+              <Image source={{uri: user.image}} style={styles.image} />
+            )}
 
-              let canUseImagePicker = false;
-              if (
-                granted === PermissionsAndroid.RESULTS.GRANTED &&
-                Platform.OS === 'android'
-              ) {
-                canUseImagePicker = true;
-              }
-              if (Platform.OS === 'ios') {
-                canUseImagePicker = true;
-              }
-
-              if (canUseImagePicker) {
-                ImagePicker.showImagePicker(options, (response) => {
-                  console.log('Response = ', response);
-
-                  if (response.didCancel) {
-                    console.log('User cancelled image picker');
-                  } else if (response.error) {
-                    console.log('ImagePicker Error: ', response.error);
-                  } else if (response.customButton) {
-                    console.log(
-                      'User tapped custom button: ',
-                      response.customButton,
-                    );
-                  } else {
-                    // const source = {uri: response.uri};
-
-                    // You can also display the image using data:
-                    const source = response;
-                    setImage(source);
-                    setImageModeUri(true);
-                    console.log(source);
-                  }
-                });
-              }
-            }}
-          />
-          <>
-            <View>
-              <Text style={styles.userName}>{user.name}</Text>
-            </View>
-          </>
-
-          <InputStyled
-            placeholder="Nombre o razon social"
-            onChange={(text) => {
-              setNombreRzb(text);
-            }}
-            style={styles.input}
-          />
-          <InputStyled
-            placeholder="Cedula o RIF"
-            onChange={(text) => {
-              setCedulaRif(text);
-            }}
-            style={styles.input}
-          />
-          <InputStyled
-            placeholder="Email"
-            onChange={(text) => {
-              setEmail(text);
-            }}
-            style={styles.input}
-          />
-          <InputStyled
-            placeholder="Telefono"
-            onChange={(text) => {
-              setPhone(text);
-            }}
-            style={styles.input}
-          />
-          <>
-            <View style={styles.paswwordChange}>
-              <Text style={styles.paswwordChange}>Cambio de clave</Text>
-            </View>
-          </>
-          <InputStyled
-            placeholder="Antigua clave"
-            secureTextEntry={true}
-            onChange={(text) => {
-              setPassword(text);
-            }}
-            style={styles.input}
-          />
-          <InputStyled
-            placeholder="Nueva clave"
-            secureTextEntry={true}
-            onChange={(text) => {
-              setRepassword(text);
-            }}
-            style={styles.input}
-          />
-          <ButtonStyled
-            onPress={() => {
-              if (repassword === password) {
-                dispatch(
-                  setSignupPartialData({
-                    nombre_razsoc: nombrerzb,
-                    cedula_rif: cedularif,
-                    correo: email,
-                    celular: phone,
-                    clave: password,
-                  }),
+            <Button
+              title="Editar foto de perfil"
+              onPress={async () => {
+                const granted = await PermissionsAndroid.request(
+                  PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                  {
+                    title: 'We need your permission',
+                  },
                 );
-                navigation.push('PlanSelect');
-              }
-            }}
-            backgroundColor={globalStyles.GRAY_COLOR}
-            color={globalStyles.WHITE_COLOR}
-            text={'Aceptar'}
-            style={styles.button}
-          />
-          <ButtonStyled
-            onPress={() => {
-              dispatch(signout());
-            }}
-            backgroundColor={globalStyles.GRAY_COLOR}
-            color={globalStyles.WHITE_COLOR}
-            text={'Cerrar sesion'}
-            style={styles.button}
-          />
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+
+                let canUseImagePicker = false;
+                if (
+                  granted === PermissionsAndroid.RESULTS.GRANTED &&
+                  Platform.OS === 'android'
+                ) {
+                  canUseImagePicker = true;
+                }
+                if (Platform.OS === 'ios') {
+                  canUseImagePicker = true;
+                }
+
+                if (canUseImagePicker) {
+                  ImagePicker.showImagePicker(options, (response) => {
+                    console.log('Response = ', response);
+
+                    if (response.didCancel) {
+                      console.log('User cancelled image picker');
+                    } else if (response.error) {
+                      console.log('ImagePicker Error: ', response.error);
+                    } else if (response.customButton) {
+                      console.log(
+                        'User tapped custom button: ',
+                        response.customButton,
+                      );
+                    } else {
+                      // const source = {uri: response.uri};
+
+                      // You can also display the image using data:
+                      const source = response;
+                      setImage(source);
+                      setImageModeUri(true);
+                      console.log(source);
+                    }
+                  });
+                }
+              }}
+            />
+
+            <InputStyled
+              placeholder="Nombre o razon social"
+              onChange={(text) => {
+                setNombreRzb(text);
+              }}
+              style={styles.input}
+            />
+            <InputStyled
+              placeholder="Cedula o RIF"
+              onChange={(text) => {
+                setCedulaRif(text);
+              }}
+              style={styles.input}
+            />
+            <InputStyled
+              placeholder="Email"
+              onChange={(text) => {
+                setEmail(text);
+              }}
+              style={styles.input}
+            />
+            <InputStyled
+              placeholder="Telefono"
+              onChange={(text) => {
+                setPhone(text);
+              }}
+              style={styles.input}
+            />
+            <>
+              <View style={styles.paswwordChange}>
+                <Text style={styles.paswwordChange}>Cambio de clave</Text>
+              </View>
+            </>
+            <InputStyled
+              placeholder="Antigua clave"
+              secureTextEntry={true}
+              onChange={(text) => {
+                setPassword(text);
+              }}
+              style={styles.input}
+            />
+            <InputStyled
+              placeholder="Nueva clave"
+              secureTextEntry={true}
+              onChange={(text) => {
+                setRepassword(text);
+              }}
+              style={styles.input}
+            />
+            <ButtonStyled
+              onPress={() => {
+                if (repassword === password) {
+                  dispatch(
+                    setSignupPartialData({
+                      nombre_razsoc: nombrerzb,
+                      cedula_rif: cedularif,
+                      correo: email,
+                      celular: phone,
+                      clave: password,
+                    }),
+                  );
+                  navigation.push('PlanSelect');
+                }
+              }}
+              backgroundColor={globalStyles.PRIMARY_COLOR}
+              color={globalStyles.WHITE_COLOR}
+              text={'Aceptar'}
+              style={styles.button}
+            />
+            <ButtonStyled
+              onPress={() => {
+                dispatch(signout());
+              }}
+              backgroundColor={globalStyles.PRIMARY_COLOR}
+              color={globalStyles.WHITE_COLOR}
+              text={'Cerrar sesion'}
+              style={styles.button}
+            />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 }
 const styles = StyleSheet.create({
