8ace3e1ab4311484d39ae54fd86d62cc0aed97e7
Oscar <oacs1221@gmail.com>
2020-07-30 21:18:55 -0400

    Quick fix iphone

--- App/assets/svg/ArrowPointer.js
+++ App/assets/svg/ArrowPointer.js
@@ -12,23 +12,23 @@ const rotation = (direction) => {
       return 'scale(-1,1)';
   }
 };
-function ArrowPointerSvg(props) {
+function ArrowPointerSvg({bold, direction, color}) {
   return (
-    <Svg viewBox="0 0 172 172" {...props}>
-      {props.bold ? (
+    <Svg viewBox="0 0 172 172">
+      {bold ? (
         <G>
           <Path
             d="M51.858 17.11a11.468 11.468 0 00-6.92 20.885L112.147 86l-67.21 48.005a11.468 11.468 0 1013.325 18.656l80.266-57.333a11.468 11.468 0 000-18.656L58.263 19.34a11.468 11.468 0 00-6.405-2.229z"
-            fill={props.color}
-            transform={rotation(props.direction ? props.direction : '')}
+            fill={color}
+            transform={rotation(direction ? direction : '')}
           />
         </G>
       ) : (
         <G>
           <Path
             d="M67.832 28.488l-4.944 4.945L115.454 86l-52.568 52.568 4.945 4.944 55.04-55.04L125.239 86l-2.366-2.473z"
-            fill={props.color}
-            transform={rotation(props.direction ? props.direction : '')}
+            fill={color}
+            transform={rotation(direction ? direction : '')}
           />
         </G>
       )}
