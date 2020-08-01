d3dc5b2cee4859616585c33e44fc720ef613fa90
Oscar <oacs1221@gmail.com>
2020-08-01 18:43:00 -0400

    Config

--- App/reducers/store.js
+++ App/reducers/store.js
@@ -1,8 +1,9 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {createEpicMiddleware} from 'redux-observable';
 import {createLogger} from 'redux-logger';
 import {rootReducer} from './index';
 import {rootEpic} from '../epics';
+import Reactotron from 'reactotron-react-native';
 const {pathname} = window.location || {};
 const IS_RUNNING_IN_CHROME = pathname && pathname.indexOf('debugger-ui');
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
@@ -16,7 +17,10 @@ export const configStore = () => {
   if (IS_RUNNING_IN_CHROME && !IS_PRODUCTION) {
     middlewares.push(logger);
   }
-  const store = createStore(rootReducer, applyMiddleware(...middlewares));
+  const store = createStore(
+    rootReducer,
+    compose(applyMiddleware(...middlewares), Reactotron.createEnhancer()),
+  );
   epicMiddleware.run(rootEpic);
   return store;
 };
