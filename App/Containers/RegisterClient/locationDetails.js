419b05ca8dcdfa8a4a3528db92aed8776301f04c
Moises Serrano <moises@alphacode.com.br>
2020-08-06 00:59:54 -0300

    Update locationDetails.js

--- App/Containers/RegisterClient/locationDetails.js
+++ App/Containers/RegisterClient/locationDetails.js
@@ -16,6 +16,8 @@ import ArrowPointerSvg from '../../assets/svg/ArrowPointer';
 import {useDispatch, useSelector} from 'react-redux';
 import {InputStyled} from '../../Components/input';
 import {resetStore} from '../../actions/utils';
+import { StyledStatusBar } from '../../Components/statusBar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export const LocationDetails = ({navigation}) => {
   const dispatch = useDispatch();
   const [direction, setDirection] = useState('');
@@ -54,66 +56,69 @@ export const LocationDetails = ({navigation}) => {
     }
   }, [message, navigation, dispatch]);
   return (
-    <ImageBackground
-      source={require('../../assets/images/wallpapers/auth.png')}
-      style={globalStyles.BACKGROUNDIMAGE}>
-      <View style={styles.view}>
-        <Header
-          navigation={navigation}
-          onPress={() => {
-            navigation.goBack();
-            dispatch(
-              setSignupPartialData({
-                latitud: null,
-                longitud: null,
-              }),
-            );
-          }}
-        />
-        <Title text={'Domicilio de Instalación'} />
-        <KeyboardAvoidingView
-          behavior="padding"
-          style={styles.keyboardContainer}
-          keyboardVerticalOffset={8}>
-          <InputStyled
-            placeholder="Dirección"
-            onChange={(text) => {
-              setDirection(text);
-            }}
-            value={direction}
-            // Icon={DirectionSvg}
-            iconColor={globalStyles.PRIMARY_COLOR}
-          />
-          <InputStyled
-            placeholder="Refpoint ..."
-            onChange={(text) => {
-              setRefpoint(text);
-            }}
-            value={refpoint}
-            // Icon={RefpointSvg}
-            iconColor={globalStyles.PRIMARY_COLOR}
-          />
-        </KeyboardAvoidingView>
-        <Text numberOfLines={2} style={styles.question}>
-          Antes de finalizar confirme si su ubicación es correcta
-        </Text>
-        <View style={styles.buttonContainer}>
-          <ButtonStyled
+    <SafeAreaView style={styles.safe}>
+      <ImageBackground
+        source={require('../../assets/images/wallpapers/auth.png')}
+        style={globalStyles.BACKGROUNDIMAGE}>
+        <View style={styles.view}>
+          <StyledStatusBar />
+          <Header
+            navigation={navigation}
             onPress={() => {
-              dispatch(signup(direction + ' ' + refpoint));
+              navigation.goBack();
+              dispatch(
+                setSignupPartialData({
+                  latitud: null,
+                  longitud: null,
+                }),
+              );
             }}
-            loading={loading}
-            disabled={loading}
-            backgroundColor={globalStyles.LIGTH_BLUE_COLOR}
-            color={globalStyles.WHITE_COLOR}
-            text={'Finalizar solicitud'}
-            styleText={styles.continueButton}
-            Icon={ArrowPointerSvg}
-            iconColor={globalStyles.WHITE_COLOR}
           />
+          <Title text={'Domicilio de Instalación'} />
+          <KeyboardAvoidingView
+            behavior="padding"
+            style={styles.keyboardContainer}
+            keyboardVerticalOffset={8}>
+            <InputStyled
+              placeholder="Dirección"
+              onChange={(text) => {
+                setDirection(text);
+              }}
+              value={direction}
+              // Icon={DirectionSvg}
+              iconColor={globalStyles.PRIMARY_COLOR}
+            />
+            <InputStyled
+              placeholder="Refpoint ..."
+              onChange={(text) => {
+                setRefpoint(text);
+              }}
+              value={refpoint}
+              // Icon={RefpointSvg}
+              iconColor={globalStyles.PRIMARY_COLOR}
+            />
+          </KeyboardAvoidingView>
+          <Text numberOfLines={2} style={styles.question}>
+            Antes de finalizar confirme si su ubicación es correcta
+          </Text>
+          <View style={styles.buttonContainer}>
+            <ButtonStyled
+              onPress={() => {
+                dispatch(signup(direction + ' ' + refpoint));
+              }}
+              loading={loading}
+              disabled={loading}
+              backgroundColor={globalStyles.LIGTH_BLUE_COLOR}
+              color={globalStyles.WHITE_COLOR}
+              text={'Finalizar solicitud'}
+              styleText={styles.continueButton}
+              Icon={ArrowPointerSvg}
+              iconColor={globalStyles.WHITE_COLOR}
+            />
+          </View>
         </View>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
