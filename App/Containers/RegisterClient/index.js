029942a8d575a938b431a38f3c40fd24770a1594
Oscar <oacs1221@gmail.com>
2020-08-05 22:24:14 -0400

    Fix avoidingview android

--- App/Containers/RegisterClient/index.js
+++ App/Containers/RegisterClient/index.js
@@ -6,6 +6,7 @@ import {
   KeyboardAvoidingView,
   ImageBackground,
   Text,
+  Platform,
 } from 'react-native';
 import {useDispatch} from 'react-redux';
 import {setSignupPartialData} from '../../actions/auth';
@@ -16,8 +17,8 @@ import {Header} from '../../Components/header';
 import {Title} from '../../Components/title';
 import globalStyles from '../../styles';
 import {Avatar} from '../../Components/avatar';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyledStatusBar } from '../../Components/statusBar';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyledStatusBar} from '../../Components/statusBar';
 
 export function RegisterScreen({navigation}) {
   const dispatch = useDispatch();
@@ -32,111 +33,111 @@ export function RegisterScreen({navigation}) {
     <SafeAreaView style={styles.safe}>
       <KeyboardAvoidingView
         behavior={'position'}
-        style={styles.keyboardContainer}
-        keyboardVerticalOffset={0}>
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : -240}
+        style={styles.keyboardContainer}>
         <ImageBackground
           source={require('../../assets/images/wallpapers/auth.png')}
           style={globalStyles.BACKGROUNDIMAGE}>
           <View style={styles.view}>
             <StyledStatusBar />
-              <Header
-                navigation={navigation}
-                onPress={() => {
-                  navigation.goBack();
-                  dispatch(
-                    setSignupPartialData({
-                      nombre_razsoc: '',
-                      cedula_rif: '',
-                      correo: '',
-                      celular: '',
-                      clave: '',
-                      avatar: '',
-                    }),
-                  );
-                }}
-              />
+            <Header
+              navigation={navigation}
+              onPress={() => {
+                navigation.goBack();
+                dispatch(
+                  setSignupPartialData({
+                    nombre_razsoc: '',
+                    cedula_rif: '',
+                    correo: '',
+                    celular: '',
+                    clave: '',
+                    avatar: '',
+                  }),
+                );
+              }}
+            />
             <Title text={'Formulario Personal'} />
             {/* <KeyboardAvoidingView
               behavior={'position'}
               style={styles.keyboardContainer}
               keyboardVerticalOffset={0}> */}
-              <ScrollView
-                style={styles.scrollView}
-                contentContainerStyle={styles.scrollViewContent}>
-                <Avatar image={image} setImage={setImage} />
-                <Text style={styles.texDivision}>
-                  Toque el icono de usuario para agregar su foto
-                </Text>
-                <InputStyled
-                  placeholder="Nombre o razon social"
-                  onChange={(text) => {
-                    setNombreRzb(text);
+            <ScrollView
+              style={styles.scrollView}
+              contentContainerStyle={styles.scrollViewContent}>
+              <Avatar image={image} setImage={setImage} />
+              <Text style={styles.texDivision}>
+                Toque el icono de usuario para agregar su foto
+              </Text>
+              <InputStyled
+                placeholder="Nombre o razon social"
+                onChange={(text) => {
+                  setNombreRzb(text);
+                }}
+                style={styles.button}
+              />
+              <InputStyled
+                placeholder="Cedula o RIF"
+                onChange={(text) => {
+                  setCedulaRif(text);
+                }}
+                style={styles.button}
+              />
+              <InputStyled
+                placeholder="Email"
+                onChange={(text) => {
+                  setEmail(text);
+                }}
+                style={styles.button}
+              />
+              <InputStyled
+                placeholder="Telefono"
+                onChange={(text) => {
+                  setPhone(text);
+                }}
+                style={styles.button}
+              />
+              <InputStyled
+                placeholder="Clave"
+                secureTextEntry={true}
+                onChange={(text) => {
+                  setPassword(text);
+                }}
+                style={styles.button}
+              />
+              <InputStyled
+                placeholder="Confirmar clave"
+                secureTextEntry={true}
+                onChange={(text) => {
+                  setRepassword(text);
+                }}
+                style={styles.button}
+              />
+              <View style={styles.buttonContainer}>
+                <ButtonStyled
+                  onPress={() => {
+                    if (repassword === password) {
+                      dispatch(
+                        setSignupPartialData({
+                          nombre_razsoc: nombrerzb,
+                          cedula_rif: cedularif,
+                          correo: email,
+                          celular: phone,
+                          clave: password,
+                          avatar: image.data,
+                        }),
+                      );
+                      navigation.push('ActualLocation');
+                    }
                   }}
-                  style={styles.button}
+                  backgroundColor={globalStyles.LIGTH_BLUE_COLOR}
+                  color={globalStyles.WHITE_COLOR}
+                  text={'Continuar'}
+                  styleText={styles.continueButton}
+                  Icon={ArrowPointerSvg}
+                  iconColor={globalStyles.WHITE_COLOR}
                 />
-                <InputStyled
-                  placeholder="Cedula o RIF"
-                  onChange={(text) => {
-                    setCedulaRif(text);
-                  }}
-                  style={styles.button}
-                />
-                <InputStyled
-                  placeholder="Email"
-                  onChange={(text) => {
-                    setEmail(text);
-                  }}
-                  style={styles.button}
-                />
-                <InputStyled
-                  placeholder="Telefono"
-                  onChange={(text) => {
-                    setPhone(text);
-                  }}
-                  style={styles.button}
-                />
-                <InputStyled
-                  placeholder="Clave"
-                  secureTextEntry={true}
-                  onChange={(text) => {
-                    setPassword(text);
-                  }}
-                  style={styles.button}
-                />
-                <InputStyled
-                  placeholder="Confirmar clave"
-                  secureTextEntry={true}
-                  onChange={(text) => {
-                    setRepassword(text);
-                  }}
-                  style={styles.button}
-                />
-                <View style={styles.buttonContainer}>
-                  <ButtonStyled
-                    onPress={() => {
-                      if (repassword === password) {
-                        dispatch(
-                          setSignupPartialData({
-                            nombre_razsoc: nombrerzb,
-                            cedula_rif: cedularif,
-                            correo: email,
-                            celular: phone,
-                            clave: password,
-                            avatar: image.data,
-                          }),
-                        );
-                        navigation.push('ActualLocation');
-                      }
-                    }}
-                    backgroundColor={globalStyles.LIGTH_BLUE_COLOR}
-                    color={globalStyles.WHITE_COLOR}
-                    text={'Continuar'}
-                    styleText={styles.continueButton}
-                    Icon={ArrowPointerSvg}
-                    iconColor={globalStyles.WHITE_COLOR}
-                  />
-                </View>
-              </ScrollView>
+              </View>
+            </ScrollView>
             {/* </KeyboardAvoidingView> */}
           </View>
         </ImageBackground>
@@ -152,7 +153,7 @@ const styles = StyleSheet.create({
   view: {
     // display: 'flex',
     // justifyContent: 'center',
-    
+
     flex: 1,
     // alignItems: 'center',
     // justifyContent: 'center',
@@ -163,7 +164,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 0,
     width: '100%',
-    overflow: 'hidden'
+    overflow: 'hidden',
     // marginBottom: '10%',
   },
   scrollView: {
